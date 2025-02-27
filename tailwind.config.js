92fac1950faa12d313875e64260ccc77ce375878
Ian McKenzie <admin@ianrandmckenzie.com>
2025-02-27 12:04:25 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {},
+      colors: {
+        cream: '#fff6e6',
+      },
+      fontFamily: {
+        serif: ['Quattrocento'],
+        mono: ['Courier'],
+        sans: ['Limelight'],
+        tagline: ['Sarina']
+      }
+    }
+  }
+}
